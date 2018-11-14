766a1688767cc98344bee89d8f98204ecedf2d8e
Matiullah Karimi <karimi@netlinks.ws>
2018-11-14 09:58:29 +0430

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:numbers_to_words/converter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String number = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+              child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter the number'
+                ),
+                keyboardType: TextInputType.number,
+                maxLength: 12,
+                onChanged: (s) {
+                  setState(() {
+                    if (s.isEmpty) {
+                      number = "";
+                      return;
+                    }
+                    number = NumberToWords.convert(int.parse(s));   
+                  });
+                },
+              ),
+              Text(
+                '$number'
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
